48561d93fda04c60ea619aa0e760d05d88f1faa3
smurf@smurf.noris.de <smurf@smurf.noris.de>
2004-05-14 15:31:18 +0200

    Renamed the package to "yapps2".

--- setup.py
+++ setup.py
@@ -7,10 +7,25 @@ from distutils.core import setup
 description = "Yet Another Python Parser System"
 long_description = \
 """
-This module implements a recursive-descent parser in Python.
+YAPPS is an easy to use parser generator that is written in Python and
+generates Python code.  There are several parser generator systems
+already available for Python, but this parser has different goals:
+Yapps is simple, very easy to use, and produces human-readable parsers.
+
+It is not the fastest or most powerful parser.  Yapps is designed to be
+used when regular expressions are not enough and other parser systems
+are too much: situations where you might otherwise write your own
+recursive descent parser.
+
+This package contains several upward-compatible enhancements to the
+original YAPPS source:
+- Handle stacked input ("include files")
+- augmented ignore-able patterns (can parse multi-line C comments correctly)
+- better error reporting
+- read input incrementally
 """
 
-setup (name = "python-shellwords",
+setup (name = "python-yapps",
        version = "2.1.1",
        description = description,
        long_description = long_description,
