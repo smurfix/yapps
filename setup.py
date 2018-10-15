bd184d53635e5c54ef7a2f7216919ba3a4abbe64
Matthias Urlichs <matthias@urlichs.de>
2018-10-15 12:37:11 +0200

    Change setup.py to point to $AUTHOR

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 from yapps import __version__ as version
 
 pkg_root = os.path.dirname(__file__)
 
 # Error-handling here is to allow package to be built w/o README included
-try: readme = open(os.path.join(pkg_root, 'README.txt')).read()
+try: readme = open(os.path.join(pkg_root, 'README.md')).read()
 except IOError: readme = ''
 
 setup(
@@ -15,15 +15,15 @@ setup(
     version = version,
     author = 'Amit J. Patel, Matthias Urlichs',
     author_email = 'amitp@cs.stanford.edu, smurf@debian.org',
-    maintainer = 'Mike Kazantsev',
-    maintainer_email = 'mk.fraggod@gmail.com',
+    maintainer = 'Matthias Urlichs',
+    maintainer_email = 'smurf@debian.org',
     license = 'MIT',
-    url = 'https://github.com/mk-fg/yapps',
+    url = 'https://github.com/smurfix/yapps',
 
     description = 'Yet Another Python Parser System',
     long_description = readme,
 
-    packages = find_packages(),
+    packages = ['yapps'],
     include_package_data = True,
     package_data = {'': ['README.txt']},
     exclude_package_data = {'': ['README.*']},
